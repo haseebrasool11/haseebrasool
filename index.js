5507c732372df9102866622ac9c6acd05cc2417b
haseebrasool11 <153386478+haseebrasool11@users.noreply.github.com>
2023-12-10 14:15:07 +0500

    file uploded portfolio
    my portfolio

---
+++ index.js
@@ -0,0 +1,64 @@
+/*==========scroll section active link==========*/
+/*let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');*/
+
+window.onscroll = () => {
+    /* sections.forEach(sec => {
+      
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight; 
+        let id = sec.getAttribute('id');
+        
+        if (top >= offset && top < offset + height) { 
+          navLinks.forEach(links => {
+        links.classList.remove('active');
+        
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+         });
+        };
+    });*/
+
+  /*==========sticky navbar==========*/
+
+let  header = document.querySelector('.header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+  };
+
+/*==========swiper==========*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  /*==========dark light mood==========*/
+
+  let darkModeIcon= document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+
+darkModeIcon.classList.toggle('bx-sun');
+document.body.classList.toggle('dark-mode');
+};
+
+/*==========scrol reveal==========*/
+
+/*ScrollReveal({
+  reset: true,
+  distance:'80px',
+  duration:2000,
+  delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{ origin: 'top'});*/
